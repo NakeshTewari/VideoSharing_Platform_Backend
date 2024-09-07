b5e7198209d2b4cfbbe75a9f774800826b0e6a53
Nakesh_Tewari <nakesh1107@gmail.com>
2024-09-07 16:20:37 +0530

    Initial commit

--- db/index.js
+++ db/index.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      // "mongodb+srv://nakesh1107:FEPjnJWI15zcjyJA@cluster0.0s6qtmm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-      process.env.MONGODB_URI
-    );
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
     console.log(
       `Mongodb connected!! Db Host: ${connectionInstance.connection.host}`
     );
